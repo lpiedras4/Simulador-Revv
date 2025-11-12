ee5ab1b8f0f55020e11694dfbe70705739c3609e
lpiedras4 <piedras.leo@gmail.com>
2025-11-12 09:42:29 -0800

    Se declararon las variables del componente vehiculo

---
+++ src/main/java/VehiculoComponent.java
@@ -0,0 +1,14 @@
+import com.almasb.fxgl.entity.component.Component;
+
+public class VehiculoComponent extends Component {
+    double masa;
+    double velocidad;
+    double posicion;
+    int desplazamiento;
+
+    int peso;
+    int tiempo;
+    double aceleracion;
+
+
+}
