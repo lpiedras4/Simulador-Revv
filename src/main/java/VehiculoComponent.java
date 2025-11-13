90a6a96fface7a7b7cbc9c60993ba367bf4b115d
lpiedras4 <piedras.leo@gmail.com>
2025-11-12 17:37:40 -0800

    Ya se definieron las variables de entrada y salida para comenzar implementacion de metodos de logica

--- src/main/java/VehiculoComponent.java
+++ src/main/java/VehiculoComponent.java
@@ -1,15 +1,26 @@
 import com.almasb.fxgl.entity.component.Component;
 
 public class VehiculoComponent extends Component {
-    double masa;
-    double velocidad;
-    double posicion;
-    double desplazamiento;
-
-    double  peso;
-    double  tiempo;
-    double aceleracion;
-    double gravedad = 9.81;
+    //Inputs iniciales del usuario
+    private double masa;
+    private double gravedad = 9.81;
+    private double peso = masa * gravedad;
+    /*Las variables fuerzaFrenadoMax y fuerzaMotorMax son definidas por el usuario y sirven como especificaciones del prototipo
+    creado */
+    private double fuerzaFrenadoMax;
+    private double fuerzaMotorMax;
+    private double coefF;
+    private double velocidadMaxSegura; //Define el limite de falla del prototipo. Si la velocidad del simulador rebasa esta variable, el prototipo se rompe
+    private double coefAero;
+    private double areaFrontal;
+    public VehiculoComponent(double masa, double fuerzaFrenado, double fuerzaMotor, double coefF, double coefAero, double areaFrontal){
+        this.masa = masa;
+        this.fuerzaFrenadoMax = fuerzaFrenadoMax;
+        this.fuerzaMotorMax = fuerzaMotorMax;
+        this.coefF = coefF;
+       this.coefAero = coefAero;
+       this.areaFrontal = areaFrontal;
+    }
 public double peso (double masa, double gravedad){
     double peso = masa * gravedad;
     return peso;
